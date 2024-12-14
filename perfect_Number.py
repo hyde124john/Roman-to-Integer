0c0dc97483f86095d6fd3d6e8dbbeb2f086b3d71
SandeepJ9849 <sandeepjennipogula114@gmail.com>
2024-12-14 23:51:09 +0530

    Create perfect_Number.py

---
+++ perfect_Number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num <= 1:
+            return False  
+        divisors_sum = 1  
+        sqrt_num = int(math.sqrt(num))
+    
+        for i in range(2, sqrt_num + 1):
+                if num % i == 0: 
+                     divisors_sum += i
+                     if i != num // i: 
+                        divisors_sum += num // i
+    
+        return divisors_sum == num
+
+        
